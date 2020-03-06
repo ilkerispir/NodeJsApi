12bb7973150210db308242e4552c9b9fc8c4c62f
ilkerispir <ikerispir@gmail.com>
2020-03-06 21:24:40 +0300

    npm express

--- server.js
+++ server.js
@@ -8,6 +8,5 @@ app.get('/', (req, res) =>{
 const server = app.listen(process.env.PORT || 8080, () => {
     const host = server.address().address;
     const port = server.address().port;
-
-    console.log('Example app listening at http://${host}:${port}');
 });
+

96e12154d9973c5a42e6ae16ff5b22c762fe0739
ilkerispir <ikerispir@gmail.com>
2020-03-18 18:07:17 +0300

    ivr

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ var getJSON = require('get-json')
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) =>{
-    getJSON('http://staging1.alo-tech.com/api/?function=reportsCDRLogs&startdate=2017-08-01%2012:00:00&finishdate=2017-08-04%2013:00:00&app_token=ag9zfnRlbGVmb25pLXRlc3RyHwsSElRlbmFudEFwcGxpY2F0aW9ucxiAgICw46OcCQyiARVzdGFnaW5nMS5hbG8tdGVjaC5jb20', function(error, response){
-        res.render('index', {data:response.CallList});
+    let id = req.query.id;
+    let url = 'https://reqres.in/api/users/' + id;
+    getJSON(url, function(error, data){
+        res.render('index', {data:data.data});
     }); 
 });
 
-const server = app.listen(process.env.PORT || 8080, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
     const host = server.address().address;
     const port = server.address().port;
 });
